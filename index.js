a3092a94fe20f56de4803be7d4a8dd9efca5a224
matthewdistefano94 <matthewdistefano94@gmail.com>
2022-05-09 23:42:42 -0400

    Add files via upload
    First commit with some working features, and no styling

---
+++ index.js
@@ -0,0 +1,62 @@
+$(document).ready(function(){
+  $('#answer-form').on('submit' ,function(e){
+    e.preventDefault();
+    checkAnswer();
+  });
+
+  $('#start-timer').on('click', function(e){
+    e.preventDefault();
+    startTimer();    
+    createAndShowMathEquation();
+    $(this).remove();
+  });
+
+});
+
+var timeLeft;
+var score = 0;
+var highScore;
+var num1, num2;
+var answer;
+
+
+var startTimer = function () {
+  timeLeft = 10;
+  var timer = setInterval(function () {
+    if (timeLeft <= 0) {
+      clearInterval(timer);
+    }
+    $('#timer').empty();
+    $('#timer').append('<p>' + timeLeft + ' Seconds Left</p');
+    timeLeft -= 1;
+  }, 1000);
+};
+
+var createAndShowMathEquation = function(){
+  //use this place to add the max variable for the bonus section
+  num1 = Math.floor(Math.random() * 9);
+  num2 = Math.floor(Math.random() * 9); 
+  var operator;
+  operator = '+';
+  var strEq = num1 + ' + ' + num2;
+  $('#equation-container').empty();
+  $('#equation-container').append('<p>' + strEq + '</p>');
+  return num1 + num2;
+};
+
+var checkAnswer = function(){
+  answer = num1 + num2; 
+  var playerInput = parseInt($('#answer-input').val());
+  if(answer === playerInput){
+    score ++;
+    timeLeft += 1;
+    console.log(timeLeft)
+    $('#current-score').empty();
+    $('#current-score').append('<p>Player Score: ' + score + '</p>');
+    createAndShowMathEquation();
+    $('#answer-input').val('');
+  }
+  else{
+    console.log('wrong');
+  }
+};
